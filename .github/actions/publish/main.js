20530740fd2153172f9b74557b9ec1d47a89b57e
Artur Finger <fingerartur@gmail.com>
2023-11-28 15:31:04 +0200

    Refactor actions/publish

--- .github/actions/publish/main.js
+++ .github/actions/publish/main.js
@@ -6,28 +6,52 @@ const core = require('@actions/core')
  * (e.g. 1.2.3-beta.4 or 1.2.3-alpha.1 etc.) then publish to a beta channel.
  */
 
+/**
+ * @typedef {'normal'|'beta'} VersionType
+ * @typedef {{ type: VersionType, label?: string }} VersionInfo
+ */
+
+/**
+ * @param {string} version version string
+ * @return {VersionInfo|null} version info or null if invalid
+ */
+function parseVersion(version) {
+  const regexBetaVersion = /^\d+\.\d+\.\d+-(\w+)\.\d+$/;
+  const regexVersion = /^\d+\.\d+\.\d+$/;
+
+  const betaMatch = version.match(regexBetaVersion);
+  if (betaMatch) {
+    const label = betaMatch[1];
+    return { type: 'beta', label };
+  }
+
+  const isNormal = regexVersion.test(version);
+  if (isNormal) {
+    return { type: 'normal' };
+  }
+
+  return null;
+}
+
+
 const version = core.getInput('version', {required: true})
-const dryRun = core.getBooleanInput('dryRun')
+const dryRun = core.getBooleanInput('dry-run')
 
-const regexBetaVersion = /^\d+\.\d+\.\d+-(\w+)\.\d+$/;
-const regexVersion = /^\d+\.\d+\.\d+$/;
+const versionInfo = parseVersion(version)
 
-const betaMatch = version.match(regexBetaVersion);
-
-if (betaMatch) {
-  const betaLabel = betaMatch[1];
-  core.info(`Publishing a Beta version ${version}.`)
-  execSync(`npm publish ${dryRun ? '--dry-run' : ''} --tag ${betaLabel}`, { stdio: 'inherit' })
-  process.exit(0);
+if (versionInfo === null) {
+  core.error(`Invalid version format. Version: ${version}`)
+  process.exit(1)
 }
 
-const isNormal = regexVersion.test(version);
-
-if (isNormal) {
-  core.info(`Publishing version ${version}.`)
-  execSync(`npm publish ${dryRun ? '--dry-run' : ''}`, { stdio: 'inherit' })
-  process.exit(0);
+const { type, label } = versionInfo;
+const args = []
+if (dryRun) {
+  args.push('--dry-run')
+}
+if (label) {
+  args.push(`--tag ${label}`)
 }
 
-core.error(`Invalid version format. Version: ${version}`);
-process.exit(1);
+core.info(`Publishing ${type === 'beta' ? 'Beta version' : 'version'} ${version}.`)
+execSync(`npm publish ${args.join(' ')}`, { stdio: 'inherit' })
